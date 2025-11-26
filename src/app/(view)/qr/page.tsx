975feffa6e9c4bb6ea8e8178ea0ed3aa472c5de3
Ksh Siaan <kshsiaan@gmail.com>
2025-11-26 17:31:35 +0600

    quick

--- src/app/(view)/qr/page.tsx
+++ src/app/(view)/qr/page.tsx
@@ -11,11 +11,11 @@ const Scanner = dynamic(
 export default function Page() {
   return (
     <Scanner
-      onScan={(result) =>
+      onScan={(result: any) =>
         toast.success(
           <pre className="bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 text-amber-400 rounded-xl p-6 shadow-lg overflow-x-auto text-sm leading-relaxed border border-zinc-700">
             <code className="whitespace-pre-wrap">
-              {JSON.stringify(result, null, 2)}
+              {JSON.stringify(result.rawValue, null, 2)}
             </code>
           </pre>
         )
