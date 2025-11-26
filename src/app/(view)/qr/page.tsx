a703f04ea7cc46982f86d490676e86ae2b17dd2a
Ksh Siaan <kshsiaan@gmail.com>
2025-11-26 17:35:36 +0600

    quick

--- src/app/(view)/qr/page.tsx
+++ src/app/(view)/qr/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         toast.success(
           <pre className="bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 text-amber-400 rounded-xl p-6 shadow-lg overflow-x-auto text-sm leading-relaxed border border-zinc-700">
             <code className="whitespace-pre-wrap">
-              {JSON.stringify(result, null, 2)}
+              {JSON.stringify(result[0]?.rawValue, null, 2)}
             </code>
           </pre>
         )
